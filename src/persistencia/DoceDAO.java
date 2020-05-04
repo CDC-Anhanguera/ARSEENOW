74b2d60e87f26ae71f40ab54bcb18ce2b8972cf1
Aryel de Sá <aryel.santos@spassu.com>
2020-05-03 21:39:27 -0300

    adicionou a logica dos metodos no DoceDAO

--- src/persistencia/DoceDAO.java
+++ src/persistencia/DoceDAO.java
@@ -7,8 +7,10 @@ package persistencia;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 import negocio.Doce;
 
 /**
@@ -43,23 +45,90 @@ public class DoceDAO implements IDoceDAO {
     }
 
     @Override
-    public void altera(Doce uma_sessao) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void altera(Doce um_doce) {
+        
+        int id = um_doce.getId_doce();
+        String sql = "UPDATE doce " + 
+                "SET nome = ?, valor = ?, quantidade = ? WHERE id_doce = " + id;
+        
+        try {
+            PreparedStatement stmt = this.connection.prepareStatement(sql);
+            
+            stmt.setString(1, um_doce.getNome());
+            stmt.setFloat(2, um_doce.getValor());
+            stmt.setInt(3, um_doce.getQuantidade());
+            
+            stmt.execute();
+            System.out.println("DOCE ALTERADO!");
+            stmt.close();
+        } catch (SQLException e){
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void remove(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = "DELETE FROM doce WHERE id_doce = " + id;
+        
+        try {
+            PreparedStatement stmt = this.connection.prepareStatement(sql);
+            
+            stmt.execute();
+            System.out.println("DOCE DELETADO!");
+            stmt.close();
+        } catch (SQLException e){
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public ArrayList<Doce> listarTodos() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try{
+            List<Doce> lista_de_doces;
+            lista_de_doces = new ArrayList<>();
+            PreparedStatement stmt = this.connection.prepareStatement("select * from psicologo");
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()){
+                Doce doce = new Doce();
+                
+                doce.setId_doce(rs.getInt("id_doce"));
+                doce.setNome(rs.getString("nome"));
+                doce.setValor(rs.getFloat("valor"));
+                doce.setQuantidade(rs.getInt("quantidade"));
+                
+                lista_de_doces.add(doce);
+            }
+            rs.close();
+            stmt.close();
+            return (ArrayList<Doce>) lista_de_doces;
+        } catch(SQLException e){
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public Doce getByID(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = "SELECT * FROM doce WHERE id_doce = " + id;
+        
+        try {
+            PreparedStatement stmt = this.connection.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            Doce doce = new Doce();
+            
+             while (rs.next()){
+                doce.setId_doce(rs.getInt("id_doce"));
+                doce.setNome(rs.getString("nome"));
+                doce.setValor(rs.getFloat("valor"));
+                doce.setQuantidade(rs.getInt("quantidade"));
+            }
+             
+            stmt.execute();
+            stmt.close();
+            return doce;
+        } catch (SQLException e){
+            throw new RuntimeException(e);
+        }
     }
 
     
